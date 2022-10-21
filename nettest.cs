ae7082e6f8227fc38bb89bee01e04c1153f6a962
MrMayankArora <Mr.Mayank.Arora@outlook.com>
2022-10-21 10:53:36 +0530

    made changes

---
+++ nettest.cs
@@ -0,0 +1,13 @@
+﻿namespace netstarttest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void DemoTest()
+    {
+        int i = 1;
+        bool result = false;
+        if (i == 1) result = true;
+        Assert.True(result, "Value should be equal to 1");
+    }
+}
