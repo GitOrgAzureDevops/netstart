f24e5bc6152b392771e67ddf348b6aa1e08b580d
myki <Mr.Mayank.Arora@outlook.com>
2022-10-21 20:16:54 +0530

    working both test

---
+++ nettest/Functions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace netstart.Module
+{
+    public class Functions
+    {
+            public int Add(int x, int y)
+            {
+                return x + y;
+            }
+    }
+}
